ec762d3ecbb3bda25894cdc8edc6255995bef426
Michael Zhukov <mishkaodessit81@gmail.com>
2026-09-23 03:03:12 +0300

    Fix mobile menu styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
-'use strict';
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.header__menu');
+const closeButton = document.querySelector('.menu__close');
+
+menuButton.addEventListener('click', () => {
+  menu.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+  menu.style.display = 'none';
+});
+
+const menuLinks = document.querySelectorAll('.menu__link');
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.style.display = 'none';
+  });
+});
